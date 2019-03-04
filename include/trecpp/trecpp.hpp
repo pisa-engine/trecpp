1e7edf89ac24349bdefdda30467260eb7f5b8304
Michal Siedlaczek <michal.siedlaczek@nyu.edu>
2019-03-04 14:47:21 -0500

    Initial commit

---
+++ include/trecpp/trecpp.hpp
@@ -0,0 +1,5 @@
+#pragma once
+
+namespace trecpp {
+    namespace detail {}
+}
---
+++ test/test_trecpp.cpp
@@ -0,0 +1,7 @@
+#define CATCH_CONFIG_MAIN
+#include "catch2/catch.hpp"
+
+#include "trecpp/trecpp.hpp"
+
+using namespace trecpp;
+using namespace trecpp::detail;
